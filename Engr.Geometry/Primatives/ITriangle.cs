5bfb360ead6e204ecae5db165b5ad82bf6e06a36
veggielane <chris@lane.org.uk>
2015-05-26 14:15:06 +0100

    changed triangle normal to float

--- Engr.Geometry/Primatives/ITriangle.cs
+++ Engr.Geometry/Primatives/ITriangle.cs
@@ -7,6 +7,6 @@ namespace Engr.Geometry.Primatives
         IVertex V1 { get; }
         IVertex V2 { get; }
         IVertex V3 { get; }
-        Vect3 Normal { get; }
+        Vect3f Normal { get; }
     }
 }
--- Engr.Geometry/Primatives/Triangle.cs
+++ Engr.Geometry/Primatives/Triangle.cs
@@ -9,10 +9,10 @@ namespace Engr.Geometry.Primatives
         public IVertex V1 { get; private set; }
         public IVertex V2 { get; private set; }
         public IVertex V3 { get; private set; }
-        public Vect3 Normal { get; private set; }
+        public Vect3f Normal { get; private set; }
 
 
-        public Triangle(IVertex v1, IVertex v2, IVertex v3, Vect3 normal):
+        public Triangle(IVertex v1, IVertex v2, IVertex v3, Vect3f normal):
             base(new[] { v1, v2, v3 }, new[] { new LineSegment(v1, v2), new LineSegment(v2, v3), new LineSegment(v3, v1) })
         {
             V1 = v1;
