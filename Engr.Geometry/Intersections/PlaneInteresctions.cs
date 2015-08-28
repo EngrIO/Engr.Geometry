6e817ab391db23569b462f989a50fb7240c94d43
veggielane <chris@lane.org.uk>
2015-08-28 21:47:31 +0100

    Same for Planes...

--- Engr.Geometry/Intersections/PlaneInteresctions.cs
+++ Engr.Geometry/Intersections/PlaneInteresctions.cs
@@ -6,7 +6,7 @@ namespace Engr.Geometry.Intersections
 {
     public static class PlaneInteresctions
     {
-        public static bool Intersects(this IPlane p, AABB aabb)
+        public static bool Intersects(this IPlane p, IAABB aabb)
         {
             var r = aabb.HalfSize.X * Math.Abs(p.Normal.X) + aabb.HalfSize.Y * Math.Abs(p.Normal.Y) + aabb.HalfSize.Z * Math.Abs(p.Normal.Z);
             var s = p.Normal.DotProduct(aabb.Center) - p.Constant;
